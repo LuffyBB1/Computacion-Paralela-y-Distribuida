63002d579bfa8c7dab01c79d17dc151e73dea582
LuffyBB <brayans.bernal@correo.usa.edu.co>
2022-03-09 23:10:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Computacion-Paralela-y-Distribuida
+ 
---
+++ Talleres/Taller #1 - Práctica de programación en C++/Punto2.cpp
@@ -0,0 +1,18 @@
+/*Materia : Computacion Paralela
+ *Fecha : 1/2/2022
+ *Autor : Brayan Esteven Bernal Martínez
+ *Tema : Taller
+ *Punto: 2
+*/
+#include <stdio.h>  
+int	main()  
+{  
+	int x = 1, y = 1;  
+	if (x == 1)  
+		if (y == 0)  
+			x = 10;  
+	else  
+		x = -1;  
+	printf("%d\n", x);  
+} 
+ 
